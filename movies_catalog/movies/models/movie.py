b96aac7d40447e0e95e3886cae932a101cdad9dd
ZHNEC77 <vadim030714@gmail.com>
2025-06-07 02:04:25 +0300

    add admin and migration

--- movies_catalog/movies/models/movie.py
+++ movies_catalog/movies/models/movie.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class Movie(models.Model):
-    class Meta:
-        ordering = ["id"]
 
     title = models.CharField(max_length=120, db_index=True)
     decription = models.TextField(blank=True, null=False)
     release_date = models.DateField(null=True, blank=True)
     duration = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ("id",)
 
     def __str__(self) -> str:
         return self.title
